650171cd170e01fe10b2c9cb1219d5b5b3fcc850
smallcoral <Shan_Hu_MC@outlook.com>
2026-03-10 22:24:04 +0800

    3.10

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -99,7 +99,8 @@ int main(void)
   while (1)
   {
     /* USER CODE END WHILE */
-
+    HAL_GPIO_WritePin(LED_GPIO_Port, LED_Pin, GPIO_PIN_SET);
+    HAL_Delay(1000);
     /* USER CODE BEGIN 3 */
   }
   /* USER CODE END 3 */
